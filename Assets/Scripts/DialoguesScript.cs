f4eea4f6f5a6525bb5e4d07790b08f6c86f55443
Nicolas Yassuda <nicolasey@al.insper.edu.br>
2024-10-28 23:46:56 -0300

    New features in Office Scene

--- Assets/Scripts/DialoguesScript.cs
+++ Assets/Scripts/DialoguesScript.cs
@@ -1,39 +1,50 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DialoguesScript : MonoBehaviour
 {
     public string[] dialogues;
-    public int dialogueIndex;
-    public GameObject dialogueBox;
+    public int dialogueIndex = 0; 
+    public GameObject dialogueBox; 
     public GameObject nextDialogueButton;
-    public TMPro.TMP_Text dialogueText;
+    public TMP_Text dialogueText; 
+
+    private Coroutine typingCoroutine;
 
     public void ResetDialogue()
     {
+        if (typingCoroutine != null)
+        {
+            StopCoroutine(typingCoroutine);
+            typingCoroutine = null;
+        }
+
         dialogueBox.SetActive(false);
         dialogueIndex = 0;
+        dialogueText.text = "";
     }
 
     public IEnumerator TypeDialogue(string dialogue)
     {
-        dialogueText.text = "";
+        dialogueText.text = ""; 
         foreach (char letter in dialogue.ToCharArray())
         {
             dialogueText.text += letter;
             yield return new WaitForSeconds(0.05f);
         }
+
+        nextDialogueButton.SetActive(true);
     }
 
     public void NextDialogue()
     {
         nextDialogueButton.SetActive(false);
 
-        if(dialogueIndex < dialogues.Length - 1)
+        if (dialogueIndex < dialogues.Length - 1)
         {
             dialogueIndex++;
-            StartCoroutine(TypeDialogue(dialogues[dialogueIndex]));
+            StartTyping(dialogues[dialogueIndex]);
         }
         else
         {
@@ -41,14 +52,26 @@ public class DialoguesScript : MonoBehaviour
         }
     }
 
-    public void showDialogue()
+    public void ShowDialogue()
     {
         dialogueBox.SetActive(true);
-        StartCoroutine(TypeDialogue(dialogues[dialogueIndex]));
+        nextDialogueButton.SetActive(false);
+
+        if (typingCoroutine != null)
+        {
+            StopCoroutine(typingCoroutine);
+        }
+
+        StartTyping(dialogues[dialogueIndex]);
     }
 
-    public void hideDialogue()
+    public void HideDialogue()
     {
         ResetDialogue();
+    }
+
+    private void StartTyping(string dialogue)
+    {
+        typingCoroutine = StartCoroutine(TypeDialogue(dialogue));
     }
 }
