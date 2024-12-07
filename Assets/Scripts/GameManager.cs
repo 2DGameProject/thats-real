998362941fbe613de0a1623c287dacc1c5cde7ed
alexandresantarossa <alexandrers3@al.insper.edu.br>
2024-12-07 15:52:06 -0300

    Ads 50%

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Advertisements;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public int score = 0;
+    public int gamePlayed = 1;
+
+
+    public bool isRewared = false;
+
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+    }
+
+    public void AddScore()
+    {
+        //if (isRewared == true)
+        //{
+        //    score = score + 2;
+        //}
+        //else 
+        //{
+        //    score++;
+        //}
+
+        score++;
+    }
+
+
+
+}
