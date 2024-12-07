e96c556334698a21282d7f5bc7e735b223f62838
PedroHrc1 <pedrohrc1@al.insper.edu.br>
2024-12-07 08:40:49 -0300

    Botoes de Moviemento, Interacao e Pause feito

---
+++ Assets/Scripts/MobileInteraction.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class MobileInteractButton : MonoBehaviour
+{
+    // Import the necessary Windows API functions
+    [DllImport("user32.dll")]
+    private static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
+
+    
+    private const uint KEYEVENTF_KEYDOWN = 0x0000; // Key press
+    private const uint KEYEVENTF_KEYUP = 0x0002;   // Key release
+
+    [Tooltip("Set the virtual key code (e.g., 0x1B for ESC, 0x45 for E)")]
+    public int virtualKeyCode = 0x45; // Default to ESC key
+
+    public void SimulateEKeyPress()
+    {
+        byte key = (byte)virtualKeyCode;
+
+        // Simulate the key down
+        keybd_event(key, 0, KEYEVENTF_KEYDOWN, UIntPtr.Zero);
+
+        // Simulate the key up
+        keybd_event(key, 0, KEYEVENTF_KEYUP, UIntPtr.Zero);
+
+        Debug.Log($"Simulated key press: 0x{key:X}");
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,13 +5,28 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
 
+    public FixedJoystick moveJoystick;
     public Animator anim;
     public float speed;
     // Update is called once per frame
     public Rigidbody2D rb;
+
+    private float moveH, moveV;
     void Update()
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+        // Check for the physical E key press
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            HandleEKeyPress();
+        }
+
+        // moveH = Input.GetAxis("Horizontal");
+        // moveV = Input.GetAxis("Vertical");
+
+        moveH = moveJoystick.Horizontal;
+        moveV = moveJoystick.Vertical;
+        
+        Vector3 movement = new Vector3(moveH, moveV, 0f);
         anim.SetFloat("Horizontal", movement.x);
         anim.SetFloat("Vertical", movement.y);
         anim.SetFloat("Speed", movement.sqrMagnitude);
@@ -19,5 +34,11 @@ public class NewBehaviourScript : MonoBehaviour
         // transform.position += movement * speed * Time.deltaTime;
         // rb.MovePosition(transform.position + movement * speed * Time.deltaTime);
         rb.velocity = new Vector2(movement.x * speed, movement.y * speed);
+    }
+
+    public void HandleEKeyPress()
+    {
+        // This is the logic that happens when E is pressed
+        Debug.Log("E key press handled!");
     }
 }
